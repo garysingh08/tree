ae9d4ffbaf58fa4726ab7883f6642576f32f9012
garysingh08 <gjagpal8@yahoo.com>
2025-05-17 22:27:58 +0000

    Added new add_nums function

--- main.cpp
+++ main.cpp
@@ -1,7 +1,12 @@
 #include<iostream>
 using namespace std;
 
+int add_nums(int a, int b){
+  return a+b;
+}
+
 int main(){
   cout << "Hello world \n";
+  cout << add_nums(3,2) << endl;
   return 0;
 }
